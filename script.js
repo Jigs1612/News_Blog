96faedc94138bcaffb0fd28f07f4224303d3d34d
Jay <55726835+Jigs1612@users.noreply.github.com>
2022-11-30 18:54:43 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+`use strict`;
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".container");
+const toggleNavbar = () => {
+  nav_header.classList.toggle("active");
+};
+
+mobile_nav.addEventListener("click", () => {
+  toggleNavbar();
+});
